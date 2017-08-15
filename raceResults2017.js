d8460ba12245950885df0df97c579459589359f9
David Thorn <thorn14@gmail.com>
2017-08-14 18:35:33 -0700

    brno and austria added, race order fixed

--- raceResults2017.js
+++ raceResults2017.js
@@ -4,7 +4,7 @@ var myChart = Highcharts.chart('raceResults2017', {
       text: '2017 Season'
   },
   xAxis: {
-      categories: ["Losail", "Termas de Rio", "COTA", "Jerez", "Le Mans", "Mugello", "Catalunya", "Assen", "Sachsenring", "Austria", "Brno", "Silverstone", "Misano", "Aragon", "Motegi", "Phillip Island", "Sepang", "Valencia"]
+      categories: ["Losail", "Termas de Rio", "COTA", "Jerez", "Le Mans", "Mugello", "Catalunya", "Assen", "Sachsenring", "Brno", "Austria", "Silverstone", "Misano", "Aragon", "Motegi", "Phillip Island", "Sepang", "Valencia"]
   },
   yAxis: {
       title: {
@@ -20,138 +20,138 @@ var myChart = Highcharts.chart('raceResults2017', {
   series: [  
     {
       name: "MARQUEZ Marc",
-      data: [13, '-', 25, 20, '-', 10, 20, 16, 25],
-      legendIndex: 129
+      data: [13, '-', 25, 20, '-', 10, 20, 16, 25, 25, 20],
+      legendIndex: 174
     },
     {
       name: "ROSSI Valentino",
-      data: [16, 20, 20, 6, '-', 13, 8, 25, 11],
-      legendIndex: 119
+      data: [16, 20, 20, 6, '-', 13, 8, 25, 11, 13, 9],
+      legendIndex: 141
     },
     {
       name: "LORENZO Jorge",
-      data: [5, '-', 7, 16, 10, 8, 13, 1, 5],
-      legendIndex: 65
+      data: [5, '-', 7, 16, 10, 8, 13, 1, 5, 1, 13],
+      legendIndex: 79
     },
     {
       name: "PEDROSA Dani",
-      data: [11, '-', 16, 25, 16, '-', 16, 3, 16],
-      legendIndex: 103
+      data: [11, '-', 16, 25, 16, '-', 16, 3, 16, 20, 16],
+      legendIndex: 139
     },
     {
       name: "VIÑALES Maverick",
-      data: [25, 25, '-', 10, 25, 20, 6, '-', 13],
-      legendIndex: 124
+      data: [25, 25, '-', 10, 25, 20, 6, '-', 13, 16, 10],
+      legendIndex: 150
     },
     {
       name: "CRUTCHLOW Cal",
-      data: ['-', 16, 13, '-', 11, '-', 5, 13, 6],
-      legendIndex: 64
+      data: ['-', 16, 13, '-', 11, '-', 5, 13, 6, 11, 1],
+      legendIndex: 76
     },
     {
       name: "DOVIZIOSO Andrea",
-      data: [20, '-', 10, 11, 13, 25, 25, 11, 8],
-      legendIndex: 123
+      data: [20, '-', 10, 11, 13, 25, 25, 11, 8, 10, 25],
+      legendIndex: 158
     },
     {
       name: "IANNONE Andrea",
-      data: ['-', 0, 9, '-', 6, 6, 0, 7, '-'],
-      legendIndex: 28
+      data: ['-', 0, 9, '-', 6, 6, 0, 7, '-', 0, 5],
+      legendIndex: 33
     },
     {
       name: "ESPARGARO Pol",
-      data: [0, 2, '-', '-', 4, '-', 0, 5, 3],
-      legendIndex: 14
+      data: [0, 2, '-', '-', 4, '-', 0, 5, 3, 7, '-'],
+      legendIndex: 21
     },
     {
       name: "BARBERA Hector",
-      data: [3, 3, 2, 4, '-', 2, 7, 0, '-'],
+      data: [3, 3, 2, 4, '-', 2, 7, 0, '-', 0, 0],
       legendIndex: 21
     },
     {
       name: "ESPARGARO Aleix",
-      data: [10, '-', 0, 7, '-', '-', '-', 6, 9],
-      legendIndex: 32
+      data: [10, '-', 0, 7, '-', '-', '-', 6, 9, 8, 3],
+      legendIndex: 43
     },
     {
       name: "REDDING Scott",
-      data: [9, 8, 4, 5, '-', 4, 3, '-', 0],
-      legendIndex: 33
+      data: [9, 8, 4, 5, '-', 4, 3, '-', 0, 0, 4],
+      legendIndex: 37
     },
     {
       name: "BAUTISTA Alvaro",
-      data: ['-', 13, 1, '-', '-', 11, 9, '-', 10],
-      legendIndex: 44
+      data: ['-', 13, 1, '-', '-', 11, 9, '-', 10, '-', 8],
+      legendIndex: 52
     },
     {
       name: "PETRUCCI Danilo",
-      data: ['-', 9, 8, 9, '-', 16, '-', 20, 4],
-      legendIndex: 66
+      data: ['-', 9, 8, 9, '-', 16, '-', 20, 4, 9, '-'],
+      legendIndex: 75
     },
     {
       name: "RINS Alex",
-      data: [7, '-', '-', '-', '-', '-', '-', 0, 0],
-      legendIndex: 7
+      data: [7, '-', '-', '-', '-', '-', '-', 0, 0, 5, 0],
+      legendIndex: 12
     },
     {
       name: "MILLER Jack",
-      data: [8, 7, 6, '-', 8, 1, '-', 10, 1],
-      legendIndex: 41
+      data: [8, 7, 6, '-', 8, 1, '-', 10, 1, 2, '-'],
+      legendIndex: 43
     },
     {
       name: "SMITH Bradley",
-      data: [0, 1, 0, 2, 3, 0, '-', '-', 2],
+      data: [0, 1, 0, 2, 3, 0, '-', '-', 2, '-', 0],
       legendIndex: 8
     },
     {
       name: "PIRRO Michele",
-      data: ['-', '-', '-', '-', '-', 7, '-', '-', '-'],
+      data: ['-', '-', '-', '-', '-', 7, '-', '-', '-','-', '-'],
       legendIndex: 7
     },
     {
       name: "RABAT Tito",
-      data: [1, 4, 3, '-', 5, 5, 1, 4, 0],
+      data: [1, 4, 3, '-', 5, 5, 1, 4, 0, 0, 0],
       legendIndex: 23
     },
     {
       name: "BAZ Loris",
-      data: [4, 5, '-', 3, 7, 0, 4, 8, 0],
-      legendIndex: 31
+      data: [4, 5, '-', 3, 7, 0, 4, 8, 0, '-', 7],
+      legendIndex: 38
     },
     {
       name: "FOLGER Jonas",
-      data: [6, 10, 5, 8, 9, 3, 10, '-', 20],
-      legendIndex: 71
+      data: [6, 10, 5, 8, 9, 3, 10, '-', 20, 6, '-'],
+      legendIndex: 77
     },
     {
       name: "GUINTOLI Sylvain",
-      data: ['-', '-', '-', '-', 1, 0, 0, '-', '-'],
+      data: ['-', '-', '-', '-', 1, 0, 0, '-', '-', '-', '-'],
       legendIndex: 1
     },
     {
       name: "KALLIO Mika",
-      data: ['-', '-', '-', '-', '-', '-', '-', '-', 0],
-      legendIndex: 0
+      data: ['-', '-', '-', '-', '-', '-', '-', '-', 0, '-', 6],
+      legendIndex: 6
     },
     {
       name: "TSUDA Takuya",
-      data: ['-', '-', '-', 0, '-', '-', '-', '-', '-'],
+      data: ['-', '-', '-', 0, '-', '-', '-', '-', '-', '-', '-'],
       legendIndex: 0
     },
     {
       name: "LOWES Sam",
-      data: [0, '-', '-', 0, 2, 0, 0, '-', '-'],
+      data: [0, '-', '-', 0, 2, 0, 0, '-', '-', 0, 0],
       legendIndex: 2
     },
     {
       name: "ZARCO Johann",
-      data: ['-', 11, 11, 13, 20, 9, 11, 2, 7],
-      legendIndex: 84
+      data: ['-', 11, 11, 13, 20, 9, 11, 2, 7, 4, 11],
+      legendIndex: 99
     },
     {
       name: "ABRAHAM Karel",
-      data: [2, 6, '-', 1, '-', 0, 2, 9, 0],
-      legendIndex: 20
+      data: [2, 6, '-', 1, '-', 0, 2, 9, 0, 3, 2],
+      legendIndex: 25
     }
   ]
 });
--- seasonResults2017.js
+++ seasonResults2017.js
@@ -4,7 +4,7 @@ var myChart = Highcharts.chart('seasonResults2017', {
       text: '2017 MotoGP Championship'
   },
   xAxis: {
-      categories: ["Losail", "Termas de Rio", "COTA", "Jerez", "Le Mans", "Mugello", "Catalunya", "Assen", "Sachsenring", "Austria", "Brno", "Silverstone", "Misano", "Aragon", "Motegi", "Phillip Island", "Sepang", "Valencia"]
+      categories: ["Losail", "Termas de Rio", "COTA", "Jerez", "Le Mans", "Mugello", "Catalunya", "Assen", "Sachsenring", "Brno", "Austria", "Silverstone", "Misano", "Aragon", "Motegi", "Phillip Island", "Sepang", "Valencia"]
   },
   yAxis: {
       title: {
@@ -21,118 +21,108 @@ var myChart = Highcharts.chart('seasonResults2017', {
   series: [  
     {
       name: "MARQUEZ Marc",
-      data: [13,13,38,58,58,68,88,104,129],
-      legendIndex: 129
+      data: [13,13,38,58,58,68,88,104,129,154,174],
+      legendIndex: 174
     },
     {
       name: "ROSSI Valentino",
-      data: [16,36,56,62,62,75,83,108,119],
-      legendIndex: 119
+      data: [16,36,56,62,62,75,83,108,119,132,141],
+      legendIndex: 141
     },
     {
       name: "LORENZO Jorge",
-      data: [5,5,12,28,38,46,59,60,65],
-      legendIndex: 65
+      data: [5,5,12,28,38,46,59,60,65,66,79],
+      legendIndex: 79
     },
     {
       name: "PEDROSA Dani",
-      data: [11,11,27,52,68,68,84,87,103],
-      legendIndex: 103
+      data: [11,11,27,52,68,68,84,87,103,123,139],
+      legendIndex: 139
     },
     {
       name: "VIÑALES Maverick",
-      data: [25,50,50,60,85,105,109,109,122],
-      legendIndex: 122
+      data: [25,50,50,60,85,105,111,111,124,140,150],
+      legendIndex: 150
     },
     {
       name: "CRUTCHLOW Cal",
-      data: [0,16,29,29,40,40,45,58,64],
-      legendIndex: 64
+      data: [0,16,29,29,40,40,45,58,64,75,76],
+      legendIndex: 76
     },
     {
       name: "DOVIZIOSO Andrea",
-      data: [20,20,30,41,54,79,104,115,123],
-      legendIndex: 123
+      data: [20,20,30,41,54,79,104,115,123,133,158],
+      legendIndex: 158
     },
     {
       name: "IANNONE Andrea",
-      data: [0,0,9,9,15,21,21,28,28],
-      legendIndex: 28
+      data: [0,0,9,9,15,21,21,28,28,28,33],
+      legendIndex: 33
     },
     {
       name: "ESPARGARO Pol",
-      data: [0,2,2,2,6,6,6,11,14],
-      legendIndex: 14
+      data: [0,2,2,2,6,6,6,11,14,21,21],
+      legendIndex: 21
     },
     {
       name: "BARBERA Hector",
-      data: [3,6,8,12,12,14,21,21,21],
+      data: [3,6,8,12,12,14,21,21,21,21,21],
       legendIndex: 21
     },
     {
       name: "ZARCO Johan",
-      data: [0,11,22,35,55,64,75,77,84],
-      legendIndex: 84
+      data: [0,11,22,35,55,64,75,77,84,88,99],
+      legendIndex: 99
     },
     {
       name: "ESPARGARO Aleix",
-      data: [10,10,10,17,17,17,17,23,32],
-      legendIndex: 32
+      data: [10,10,10,17,17,17,17,23,32,40,43],
+      legendIndex: 43
     },
     {
       name: "REDDING Scott",
-      data: [0],
-      legendIndex: 0
+      data: [9,17,21,26,26,30,33,33,33,33,37],
+      legendIndex: 37
     },
     {
       name: "BAUTISTA Alvaro",
-      data: [0],
-      legendIndex: 0
+      data: [0,13,14,14,14,25,34,34,44,44,52],
+      legendIndex: 52
     },
     {
       name: "PETRUCCI Danilo",
-      data: [0,9,17,26,26,42,42,62,66],
-      legendIndex: 66
+      data: [0,9,17,26,26,42,42,62,66,75,75],
+      legendIndex: 75
     },
     {
       name: "MILLER Jack",
-      data: [0],
-      legendIndex: 0
+      data: [8,15,21,21,29,30,30,40,41,43,43],
+      legendIndex: 43
     },
     {
       name: "SMITH Bradley",
-      data: [0],
-      legendIndex: 0
+      data: [0,1,1,3,6,6,6,6,8,8,8],
+      legendIndex: 8
     },
     {
       name: "PIRRO Michele",
-      data: [0],
-      legendIndex: 0
+      data: [0,0,0,0,0,7,7,7,7,7,7],
+      legendIndex: 7
     },
     {
       name: "RABAT Tito",
-      data: [0],
-      legendIndex: 0
+      data: [1,5,8,8,13,18,19,23,23,23,23],
+      legendIndex: 23
     },
     {
       name: "BAZ Loris",
-      data: [0],
-      legendIndex: 0
-    },
-    {
-      name: "HERNANDEZ Yonny",
-      data: [0],
-      legendIndex: 0
-    },
-    {
-      name: "LOWES Alex",
-      data: [0],
-      legendIndex: 0
+      data: [4,9,9,12,19,19,23,31,31,31,38],
+      legendIndex: 38
     },
     {
       name: "FOLGER Jonas",
-      data: [6,16,21,29,38,41,51,51,71],
-      legendIndex: 71
+      data: [6,16,21,29,38,41,51,51,71,77,77],
+      legendIndex: 77
     }
   ]
 });
